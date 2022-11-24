520093c625a93d12a11c63e6ca91e5edfa00c4bd
Glen Nguyen <glendle.nguyen@gmail.com>
2022-11-24 13:12:33 +0800

    installed tailwind css and configs

---
+++ nuxt-wadl/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./components/**/*.vue",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.js",
+    "./nuxt.config.js",
+    "./app.vue",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
