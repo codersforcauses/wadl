51db495722102d80e2c50c5f202b3d28c80c6b93
Ivan <ivan_peh@hotmail.com>
2022-11-28 16:27:18 +0800

    Link to Google Fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    css: ['~/assets/css/main.css'],
-    postcss: {
-        plugins: {
-            tailwindcss: {},
-            autoprefixer: {},
-        },
+  css: ["~/assets/css/main.css"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
     },
-})
+  },
+  app: {
+    head: {
+      link: [
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css?family=Lato&family=Jost",
+        },
+      ],
+    },
+  },
+});
