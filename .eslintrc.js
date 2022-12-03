71531040c2ef1bb294f9138783670712bbc82fc8
rezbyte <48037381+rezbyte@users.noreply.github.com>
2022-12-03 15:10:26 +0800

    Use Vue's eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,11 @@
 module.exports = {
   root: true,
-  env: {
-    browser: true,
-    node: true, // Allow __dirname & other Vite/Node.js variables
-    es2021: true
-  },
   extends: [
     'standard-with-typescript',
     'plugin:vue/vue3-recommended',
+    '@vue/eslint-config-standard-with-typescript',
     'plugin:prettier/recommended',
   ],
-  overrides: [
-  ],
-  parser: 'vue-eslint-parser',
-  parserOptions: {
-    parser: '@typescript-eslint/parser',
-    project: './tsconfig.json',
-    extraFileExtensions: ['.vue'],
-    ecmaVersion: 'latest',
-    sourceType: 'module'
-  },
   rules: {
     "vue/multi-word-component-names": 0,
   }
