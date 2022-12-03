347577e4bad54fa1989bfdbff1cc8a66915eb35f
Nicholas Davies <nicholasjohndavies42@gmail.com>
2022-12-03 16:15:35 +0800

    process.env -> import.meta.env

--- src/components/Firebase/init.ts
+++ src/components/Firebase/init.ts
@@ -6,12 +6,12 @@ import {
 } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID
+  apiKey: import.meta.env.API_KEY,
+  authDomain: import.meta.env.AUTH_DOMAIN,
+  projectId: import.meta.env.PROJECT_ID,
+  storageBucket: import.meta.env.STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.MESSAGING_SENDER_ID,
+  appId: import.meta.env.APP_ID
 }
 
 let app: FirebaseApp
