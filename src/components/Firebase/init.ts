38e8ec79bfc3be26419f60c5b8df5e70275aef41
Nicholas Davies <nicholasjohndavies42@gmail.com>
2022-12-04 23:20:52 +0800

    init.ts fix - runs both server and client

--- src/components/Firebase/init.ts
+++ src/components/Firebase/init.ts
@@ -1,28 +1,38 @@
-import { FirebaseApp, getApp, getApps, initializeApp } from 'firebase/app'
+import { FirebaseApp, getApp, getApps, initializeApp } from "firebase/app";
+import { Firestore, getFirestore } from "firebase/firestore";
 
-import {
-  Firestore,
-  getFirestore
-} from 'firebase/firestore'
+let app: FirebaseApp;
+let db: Firestore;
 
-const firebaseConfig = {
-  apiKey: import.meta.env.API_KEY,
-  authDomain: import.meta.env.AUTH_DOMAIN,
-  projectId: import.meta.env.PROJECT_ID,
-  storageBucket: import.meta.env.STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.MESSAGING_SENDER_ID,
-  appId: import.meta.env.APP_ID
-}
+let clientSide: boolean = process.client;
 
-let app: FirebaseApp
-let db: Firestore
-
-const clientSide = typeof window !== 'undefined'
-
-// Runs on the client side
 if (clientSide) {
-  app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp()
-  db = getFirestore(app)
-}
+  const config = useRuntimeConfig();
+  // Runs on the client side
+  console.log("config is: \n\n");
+  console.log(config.firebaseApiKey);
 
-export { db }
+  const firebaseConfig = {
+    apiKey: config.firebaseApiKey,
+    authDomain: config.firebaseAuthDomain,
+    projectId: config.firebaseProjectId,
+    storageBucket: config.firebaseStorageBucket,
+    messagingSenderId: config.firebaseMessagingSenderId,
+    appId: config.firebaseAppId,
+  };
+  app = initializeApp(firebaseConfig);
+  db = getFirestore(app);
+} else {
+  // TODO: useRunTimeConfig does not work on server side - docs say it should.
+  const firebaseConfig = {
+    apiKey: process.env.firebaseApiKey,
+    authDomain: process.env.firebaseAuthDomain,
+    projectId: process.env.firebaseProjectId,
+    storageBucket: process.env.firebaseStorageBucket,
+    messagingSenderId: process.env.firebasemassagingId,
+    appId: process.env.firebaseAppId,
+  };
+  app = initializeApp(firebaseConfig);
+  db = getFirestore(app);
+}
+export { db };
