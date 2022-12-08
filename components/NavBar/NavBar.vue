320c6dfa88f7512688bcc27671178258e99cfdcb
Michael <m.m.downing@bigpond.com>
2022-12-09 00:35:32 +0800

    made navbar sticky

--- components/NavBar/NavBar.vue
+++ components/NavBar/NavBar.vue
@@ -1,5 +1,7 @@
 <template>
-  <nav class="bg-gold shadow flex position:relative place-items-center">
+  <nav
+    class="bg-gold shadow flex position:relative place-items-center sticky top-0 z-50"
+  >
     <HomeButton class="pl-3" />
     <LoginButton class="absolute right-3" button-text="Login" />
   </nav>
