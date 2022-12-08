2c4264f7482343e49649e27cb152d7d950e2c28a
benvarian <88700858+benvarian@users.noreply.github.com>
2022-12-08 21:23:43 +0800

    setup firestore emulator to run if the .env value for mode is dev

--- src/Firebase/init.js
+++ src/Firebase/init.js
@@ -1,5 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
+import { getAuth, connectAuthEmulator } from "firebase/auth";
+
 
 
 let app;
