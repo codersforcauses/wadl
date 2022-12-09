4ef8e47ceb451afba6943766faae74c18b88a7a1
ChantelleKerr <chantellekerr93@gmail.com>
2022-12-09 21:39:40 +0800

    added comments

--- pages/components.vue
+++ pages/components.vue
@@ -1,8 +1,10 @@
-<!-- This is the be removed before production -->
 <!-- A page that demo's how to use different components. -->
+<!-- This is to be removed before production -->
 <!-- If you have a component that will be used across multiple pages add it here -->
 <template>
+  <!-- Header Component -->
   <Header title-text="Website Components" />
+  <!-- Button Component -->
   <div class="flex flex-row gap-4 justify-center mt-8 mx-2">
     <Button button-text="Undo" button-color="bg-gray-400" @click="handleUndo" />
     <Button button-text="Save" button-color="bg-gold" @click="handleDelete" />
