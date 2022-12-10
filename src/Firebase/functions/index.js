f5a40d5121c07f2b39735e8e12d1ac25500276af
Ivan <ivan_peh@hotmail.com>
2022-12-10 14:09:03 +0800

    load killswitch into index.js

--- src/Firebase/functions/index.js
+++ src/Firebase/functions/index.js
@@ -1,6 +1,6 @@
 const functions = require("firebase-functions");
-
-// // Create and deploy your first functions
+const killswitch = require("./billingKillswitch");
+exports.killswitch = killswitch.stopBilling; // // Create and deploy your first functions
 // // https://firebase.google.com/docs/functions/get-started
 //
 // exports.helloWorld = functions.https.onRequest((request, response) => {
