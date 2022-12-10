7ae872b1d578ed023c4c9b5422a7dff7e298f0b6
rezbyte <48037381+rezbyte@users.noreply.github.com>
2022-12-10 14:57:48 +0800

    Linted

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,20 @@
 <!-- eslint-disable vue/attribute-hyphenation -->
 <template>
-  
   <!-- long logo -->
   <img class="mx-auto" src="../assets/logos/TransparentLongLogo.png" />
   <!-- competitions -->
-  <index titleText="Competitions"/>
-  <br>
-  <div class="mx-auto h-screen place-items-center justify-center grid grid-cols-3 grid-rows-6 gap-6">
-      <!-- replace key with item.key when final data exists -->
-      <div v-for="item in tournaments" :key="item" >
-        <NuxtLink to="/">
-          <LevelButton :text="item" />
-        </NuxtLink>
-      </div>
+  <index titleText="Competitions" />
+  <br />
+  <div
+    class="mx-auto h-screen place-items-center justify-center grid grid-cols-3 grid-rows-6 gap-6"
+  >
+    <!-- replace key with item.key when final data exists -->
+    <div v-for="item in tournaments" :key="item">
+      <NuxtLink to="/">
+        <LevelButton :text="item" />
+      </NuxtLink>
     </div>
-  
+  </div>
 </template>
 
 <script setup>
