859094e1f6e9132bedb69e79f3ca65a8f4acb288
David <david.holt2002@gmail.com>
2022-12-10 13:48:43 +0800

    progressing

--- pages/superAdminPage.vue
+++ pages/superAdminPage.vue
@@ -1,17 +1,18 @@
 <script setup>
-    import AdminButton from '~/components/AdminButton.vue';
-    import ProfileInfo from '~/components/ProfileInfo.vue';
+    import AdminButton from '~/components/SuperAdminPage/AdminButton.vue';
+    import ProfileInfo from '~/components/SuperAdminPage/ProfileInfo.vue';
 </script>
 
 <template>
     <div class="flex items-center justify-center">
         <ProfileInfo username="Carmen Leong" role="Super Admin"/>
     </div>
+    <hr class="border-2 border-black border-solid size-8">
     <div class="flex items-center justify-center w-full">
         <div class="grid w-2/3 grid-cols-3 gap-2">
             <AdminButton title="Tournaments" link="/" />
             <AdminButton title="Institutions" link="/" />
-            <AdminButton title="Adjudicators" link="/"/>
+            <AdminButton title="Adjudicators" link="/Adjudicators"/>
             <AdminButton title="Teams" link="/"/>
             <AdminButton title="Venues" link="/"/>
             <AdminButton title="Fixtures" link="/"/>
@@ -21,7 +22,3 @@
         </div>
     </div>
 </template>
-
-<style>
-
-</style>
