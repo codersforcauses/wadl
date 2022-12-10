d1d13e0798077d6d8cb1d860a69663867119cae9
LemonTheSour <m.m.downing@bigpond.com>
2022-12-10 14:08:33 +0800

    header added

---
+++ components/Manage-Venue/venue.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>{{venue[0].venue}}</div>
+</template>
+<script setup>
+    const venues = [
+        {
+            venue: "South Street",
+            roomNum: "024",
+            day: "Wednesday"
+        },
+        {
+            venue: "Adjacent Avenue",
+            roomNum: "96",
+            day: "Wednesday"
+        },
+        {
+            venue: "Rightmost Road",
+            roomNum: "111",
+            day: "Wednesday"
+        },
+    ]
+</script>
--- pages/manage-venue.vue
+++ pages/manage-venue.vue
@@ -1,2 +1,33 @@
-<template></template>
-<script setup></script>
+<template>
+  <div>
+    <h1 class="text-center text-4xl pt-5">Venues</h1>
+  </div>
+  
+  <hr class="my-5 w-4/5 h-px mx-auto bg-black border-0" />
+
+  <div>
+    <venue />
+  </div>
+
+</template>
+<script setup>
+import venue from '../components/Manage-Venue/venue.vue';
+
+const venues = [
+        {
+            venue: "South Street",
+            roomNum: '024',
+            day: "Wednesday"
+        },
+        {
+            venue: "Adjacent Avenue",
+            roomNum: '96',
+            day: "Wednesday"
+        },
+        {
+            venue: "Rightmost Road",
+            roomNum: '111',
+            day: "Wednesday"
+        },
+    ]
+    </script>
