41359de7d70324e0bc3ef578c24528e39e5bd871
Michael <m.m.downing@bigpond.com>
2022-12-12 09:25:05 +0800

    table component WIP

---
+++ components/Table/index.vue
@@ -0,0 +1,32 @@
+<!-- eslint-disable vue/require-valid-default-prop -->
+<template>
+  <table>
+    <thead>
+      <tr>
+        <th v-for="(object, index) in config" :key="index" class="py-3">
+          {{ object.title }}
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(row, index) in venues" :key="index">
+        <td v-for="(object, ind) in config" :key="ind">
+          {{ row[object.key] }}
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script setup>
+defineProps({
+  config: {
+    type: Object,
+    default: [],
+  },
+  venues: {
+    type: Object,
+    default: [],
+  },
+});
+</script>
--- pages/manage-venue.vue
+++ pages/manage-venue.vue
@@ -1,34 +1,43 @@
+<!-- eslint-disable vue/attribute-hyphenation -->
 <template>
-  
-  <pageHeader titleText="Venues" />
-  
-  <div v-for="venue in venues">
-    <venue :venue="venue" />
+  <Header titleText="Venues" />
+  <div class="flex content-center justify-center">
+    <Table :config="config" :venues="venues" />
   </div>
-
 </template>
 <script setup>
-import venue from "../components/Manage-Venue/venue.vue"
-import pageHeader from "../components/Header/index.vue"
-
 const venues = [
-        {
-            index: "1",
-            venue: "South Street",
-            roomNum: "024",
-            day: "Wednesday"
-        },
-        {
-            index: "2",
-            venue: "Adjacent Avenue",
-            roomNum: "96",
-            day: "Wednesday"
-        },
-        {
-            index: "3",
-            venue: "Rightmost Road",
-            roomNum: "111",
-            day: "Wednesday"
-        },
-    ]
+  {
+    index: "1",
+    venue: "South Street",
+    day: "Wednesday",
+    roomNum: "024",
+  },
+  {
+    index: "2",
+    venue: "Adjacent Avenue",
+    day: "Wednesday",
+    roomNum: "96",
+  },
+  {
+    index: "3",
+    venue: "Rightmost Road",
+    day: "Wednesday",
+    roomNum: "111",
+  },
+];
+const config = [
+  {
+    key: "venue",
+    title: "Venue",
+  },
+  {
+    key: "day",
+    title: "Day",
+  },
+  {
+    key: "roomNum",
+    title: "Room No.",
+  },
+];
 </script>
