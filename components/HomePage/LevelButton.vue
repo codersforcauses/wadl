d97ae993f11ab0d346dc07a31f599decf167e5eb
unknown <23225186@student.uwa.edu.au>
2022-12-12 20:20:05 +0800

    Change div into button in button component

--- components/HomePage/LevelButton.vue
+++ components/HomePage/LevelButton.vue
@@ -1,9 +1,9 @@
 <template>
-  <div
+  <button
     class="bg-black p-2 m-5 w-64 h-20 rounded-2xl flex justify-center place-items-center text-white text-3xl font-montserrat"
   >
     {{ text }}
-  </div>
+</button>
 </template>
 
 <script setup>
