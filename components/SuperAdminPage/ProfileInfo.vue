553419661ff79e1e508cc6c294b014ee6741db92
David <david.holt2002@gmail.com>
2022-12-13 11:00:55 +0800

    Moved profile symbol to profileinfo page page - linting not done yet

--- components/SuperAdminPage/ProfileInfo.vue
+++ components/SuperAdminPage/ProfileInfo.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { UserIcon } from "@heroicons/vue/24/outline";
         const {props} = defineProps({
         username: String,
         role: String,
@@ -6,9 +7,12 @@
 </script>
 
 <template>
-    <div class="w-2/3">
-        <div class="text-6xl">{{username}}</div>
-        <div class="mt-3 ml-6 text-xl">{{role}}</div>
-        <hr class="w-full mt-5 mb-5 border-black border-solid">
+    <div class="flex items-center justify-center">        
+        <UserIcon class="w-28 h-28" />
+        <div class="w-2/3">
+            <div class="text-6xl">{{username}}</div>
+            <div class="mt-3 ml-6 text-xl">{{role}}</div>
+            <hr class="w-full mt-5 mb-5 border-black border-solid">
+        </div>
     </div>
 </template>
--- pages/superAdminPage.vue
+++ pages/superAdminPage.vue
@@ -1,15 +1,12 @@
 <script setup>
     import AdminButton from '~/components/SuperAdminPage/Admin.vue';
     import ProfileInfo from '~/components/SuperAdminPage/ProfileInfo.vue';
-    import { UserIcon, TrophyIcon, BuildingLibraryIcon, ScaleIcon, UserGroupIcon, 
+    import {TrophyIcon, BuildingLibraryIcon, ScaleIcon, UserGroupIcon, 
             MapPinIcon, TableCellsIcon, UsersIcon, ClipboardIcon, UserPlusIcon } from "@heroicons/vue/24/outline";
 </script>
 
 <template>
-    <div class="flex items-center justify-center">
-        <UserIcon class="w-28 h-28" />
-        <ProfileInfo username="Carmen Leong" role="Admin"/>
-    </div>
+    <ProfileInfo username="Carmen Leong" role="Admin"/>
     <div class="flex items-center justify-center w-full">
         <div class="grid w-2/3 grid-cols-3 gap-2">
             <AdminButton title="Tournaments" link="/" :icon="TrophyIcon" />
