88866b26fce79cb061cf87b69ae52ecbefbf6d47
David <david.holt2002@gmail.com>
2022-12-13 16:48:14 +0800

    Profile component

---
+++ pages/coordinator.vue
@@ -0,0 +1,9 @@
+<script setup>
+    import ProfileInfo from "~/components/admin/ProfileInfo.vue";
+</script>
+
+<template>
+<div>
+    <ProfileInfo username="Carmen Leong" role="Admin" />
+</div>
+</template>
