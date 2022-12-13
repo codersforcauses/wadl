cd1b54bf28c4f9afbb9a0b453e4d953be9c8051a
David <david.holt2002@gmail.com>
2022-12-13 11:17:43 +0800

    Linting committed and name change to Admin page

---
+++ pages/AdminPage.vue
@@ -0,0 +1,36 @@
+<script setup>
+import AdminButton from "~/components/AdminPage/AdminButton.vue";
+import ProfileInfo from "~/components/AdminPage/ProfileInfo.vue";
+import {
+  TrophyIcon,
+  BuildingLibraryIcon,
+  ScaleIcon,
+  UserGroupIcon,
+  MapPinIcon,
+  TableCellsIcon,
+  UsersIcon,
+  ClipboardIcon,
+  UserPlusIcon,
+} from "@heroicons/vue/24/outline";
+</script>
+
+<template>
+  <ProfileInfo username="Carmen Leong" role="Admin" />
+  <div class="flex items-center justify-center w-full">
+    <div class="grid w-2/3 grid-cols-3 gap-2">
+      <AdminButton title="Tournaments" link="/" :icon="TrophyIcon" />
+      <AdminButton title="Institutions" link="/" :icon="BuildingLibraryIcon" />
+      <AdminButton
+        title="Adjudicators"
+        link="/Adjudicators"
+        :icon="ScaleIcon"
+      />
+      <AdminButton title="Teams" link="/" :icon="UserGroupIcon" />
+      <AdminButton title="Venues" link="/" :icon="MapPinIcon" />
+      <AdminButton title="Fixtures" link="/" :icon="TableCellsIcon" />
+      <AdminButton title="Users" link="/" :icon="UsersIcon" />
+      <AdminButton title="Results" link="/" :icon="ClipboardIcon" />
+      <AdminButton title="Sign-up Requests" link="/" :icon="UserPlusIcon" />
+    </div>
+  </div>
+</template>
