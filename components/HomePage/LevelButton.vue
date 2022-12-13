a4fde504d2119110dc584763eeadc7e3f06ffdba
David Wang <101696493+daviid-wang@users.noreply.github.com>
2022-12-13 16:12:05 +0800

    I12 super admin page (#67)
    * Working on super admin page
    
    * Basic AdminButton component
    
    * new format
    
    * Yellow box styled
    
    * center everything
    
    * progressing
    
    * logos + nice overall design
    
    * All changes done except linting
    
    * Moved profile symbol to profileinfo page page - linting not done yet
    
    * Linting committed and name change to Admin page
    
    * More name changes
    
    * Linted
    
    Co-authored-by: rezbyte <48037381+rezbyte@users.noreply.github.com>

--- components/HomePage/LevelButton.vue
+++ components/HomePage/LevelButton.vue
@@ -3,7 +3,7 @@
     class="bg-black p-2 m-5 w-64 h-20 rounded-2xl flex justify-center place-items-center text-white text-3xl font-montserrat"
   >
     {{ text }}
-</button>
+  </button>
 </template>
 
 <script setup>
