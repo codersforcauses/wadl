4c989d972ae92c4ef5d778f885887d7a935331c9
David <david.holt2002@gmail.com>
2022-12-14 16:05:16 +0800

    Basic v-for loop to make headings

--- components/Coordinator/TableHeader.vue
+++ components/Coordinator/TableHeader.vue
@@ -1,5 +1,29 @@
 <script setup>
 defineProps({
-
+    headings: Array,
+    Default: [],
 })
 </script>
+
+<template>
+    <div class="flex justify-center">
+        <div class="w-2/3">
+            <table class="flex items-center justify-center table-auto">
+                <thead>
+                    <tr>
+                        <th class="text-2xl text-black" v-for="({ heading }, index) in headings">
+                            {{ headings[index] }}
+                            <!-- {{ index }} -->
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    
+                </tbody>
+            </table>
+        </div>
+        <div>
+            
+        </div>
+    </div>
+</template>
--- pages/coordinator.vue
+++ pages/coordinator.vue
@@ -1,9 +1,15 @@
 <script setup>
     import ProfileInfo from "~/components/admin/ProfileInfo.vue";
+    import TableHeader from "~/components/coordinator/TableHeader.vue";
 </script>
 
 <template>
 <div>
-    <ProfileInfo username="Carmen Leong" role="Admin" />
+    <div>
+        <ProfileInfo username="Carmen Leong" role="Admin" />
+    </div>
+    <div>
+        <TableHeader :headings="['Teams', 'Level', 'Division']" />
+    </div>
 </div>
 </template>
