941f61ea96ae5fb863fcb0c2a2d3cf2790acbf1a
Ivan <ivan_peh@hotmail.com>
2022-12-14 14:42:14 +0800

    changed to tailwind

--- components/SignUpForm/DefaultInputs.vue
+++ components/SignUpForm/DefaultInputs.vue
@@ -1,38 +1,47 @@
 <template>
   <div>
-    <p>Name</p>
+    <p class="mb-1">Name</p>
     <div class="grid grid-cols-2 gap-x-4">
-      <input type="text" id="fname" placeholder="First Name" required />
-      <input type="text" id="lname" placeholder="Last Name" required />
+      <input
+        type="text"
+        id="fname"
+        placeholder="First Name"
+        required
+        class="p-1 pl-2.5 mb-2.5 border border-solid border-light-grey rounded-md w-full placeholder:heading-montserrat"
+      />
+      <input
+        type="text"
+        id="lname"
+        placeholder="Last Name"
+        required
+        class="p-1 pl-2.5 mb-2.5 border border-solid border-light-grey rounded-md w-full placeholder:heading-montserrat"
+      />
     </div>
-    <p>Email</p>
-    <input type="text" id="email" placeholder="Your Email" required />
-    <p>Password</p>
-    <input type="password" id="password" placeholder="Your Password" required />
-    <p>Confirm Password</p>
+    <p class="mb-1">Email</p>
+    <input
+      type="text"
+      id="email"
+      placeholder="Your Email"
+      required
+      class="p-1 pl-2.5 mb-2.5 border border-solid border-light-grey rounded-md w-full placeholder:heading-montserrat"
+    />
+    <p class="mb-1">Password</p>
+    <input
+      type="password"
+      id="password"
+      placeholder="Your Password"
+      required
+      class="p-1 pl-2.5 mb-2.5 border border-solid border-light-grey rounded-md w-full placeholder:heading-montserrat"
+    />
+    <p class="mb-1">Confirm Password</p>
     <input
       type="password"
       id="password"
       placeholder="Confirm Password"
       required
+      class="p-1 pl-2.5 mb-2.5 border border-solid border-light-grey rounded-md w-full placeholder:heading-montserrat"
     />
   </div>
 </template>
 
-<style scoped>
-p {
-  margin-bottom: 4px;
-}
-input {
-  padding: 4px;
-  padding-left: 10px;
-  border: solid 1px lightgray;
-  border-radius: 6px;
-  margin-bottom: 10px;
-  width: 100%;
-}
-::placeholder {
-  font-family: theme("fontFamily.brandon");
-  letter-spacing: theme("letterSpacing.12");
-}
-</style>
+<style scoped></style>
