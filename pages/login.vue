a3391f1090d3bcf177788e8b6498515b4897c871
Nadun Vindiya <nadunbalasuriya03@gmail.com>
2022-12-17 13:18:40 +0800

    Made changes to the layout

--- pages/login.vue
+++ pages/login.vue
@@ -45,29 +45,27 @@ Include the the login error into the compositon style API -->
 
 
 <template>
-    <div class="text-center bg-gold w-[550px] h-[600px] pt-2.5 pb-2.5 mt-24 border border-light-grey rounded-xl flex flex-col content-center m-auto flex-shrink-1">
+  <div class="flex justify-center h-screen w-screen">
+    <div class=" text-center bg-gold border w-3/12 h-3/4 border-light-grey rounded-xl content-center my-auto p-10">
       <h1 class="text-5xl mt-9 mb-10">Login</h1>
       <br>
       <form @submit.prevent="handleLogin">
-        <label >
-          Email
-          <br>
-          <input type="email" v-model="email" class="border border-light-grey rounded-md h-[50px] w-[450px] justify-self-center self-center" placeholder=" Your Email"/>
-        </label>
-        <br><br>
         <label>
-          Password
-          <br>
-          <input type="password" v-model="password" class="border border-light-grey rounded-md h-[50px] w-[450px]" placeholder=" Your Password"/>
+          Email
         </label>
+        <input type="email" v-model="email" class="border border-light-grey rounded-md h-[50px] w-[450px] justify-self-center self-center" placeholder=" Your Email"/>
+        <label>
+          Password    
+        </label>
+        <input type="password" v-model="password" class="border border-light-grey rounded-md h-[50px] w-[450px]" placeholder=" Your Password"/>
         <br><br>
-        <!-- <button type="submit" class="button">Submit</button> -->
         <Button class="mt-[60px] h-[60px] w-[200px] rounded-xl"
         button-text="Submit"
         button-color="bg-white" />
-        <p v-if="error" class="red">{{ error }}</p>
-        
+        <p v-if="error" class="text-red">{{ error }}</p>
+        <NuxtLink to="/"> <p class="underline mt-5">Forgot my password</p> </NuxtLink>
       </form>
+    </div>
     </div>
 </template>
   
