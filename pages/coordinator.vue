9f8ac4900586553946b4a5d8fc9b4da9dc57ef81
David <david.holt2002@gmail.com>
2022-12-17 15:19:35 +0800

    CAPITAL T

--- pages/coordinator.vue
+++ pages/coordinator.vue
@@ -1,6 +1,6 @@
 <script setup>
 import ProfileInfo from "~/components/admin/ProfileInfo.vue";
-import Table from "~/components/Coordinator/table.vue";
+import Table from "~/components/Coordinator/Table.vue";
 </script>
 
 <template>
