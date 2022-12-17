f43121ce6a2851b7e464e7ed8502779ca29f99ae
rezbyte <48037381+rezbyte@users.noreply.github.com>
2022-12-17 13:43:37 +0800

    Enabled Cypress Studio & Set BaseURL

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,8 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  experimentalStudio: true,
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl: "http://localhost:3000",
   },
 });
