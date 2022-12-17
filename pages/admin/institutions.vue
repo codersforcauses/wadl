3c4a45cc82b0cf9982eac5927e3ca65e43f72696
Muffin-Man2 <85885968+Muffin-Man2@users.noreply.github.com>
2022-12-17 14:43:03 +0800

    Small CSS change 😮‍💨

--- pages/admin/institutions.vue
+++ pages/admin/institutions.vue
@@ -53,7 +53,7 @@ const deleteInstitution = () => {
     v-show="modalVisibility"
     class="flex justify-center items-center fixed top-0 z-50 left-0 w-full h-full bg-slate-300 bg-opacity-60"
   >
-    <div class="bg-white max-w-[40%] min-w-[400px] rounded-2xl">
+    <div class="bg-white max-w-[40%] min-w-[320px] rounded-2xl">
       <header
         class="flex justify-end items-center pt-2 pr-2 pb-2 bg-gold rounded-t-2xl"
       >
