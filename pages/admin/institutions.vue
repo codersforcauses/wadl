18bcdc3f60faa7ea0a014275afe7f07ce40cd135
Muffin-Man2 <85885968+Muffin-Man2@users.noreply.github.com>
2022-12-17 13:29:57 +0800

    Modal popup working

--- pages/admin/institutions.vue
+++ pages/admin/institutions.vue
@@ -1,9 +1,10 @@
 <script setup>
 import { ref } from "vue";
-import { PencilIcon } from "@heroicons/vue/24/solid";
+import { XMarkIcon } from "@heroicons/vue/24/solid";
 import data from "../../data/institutions.json";
 
 const institutions = ref(data);
+var modal_visibility = ref(true);
 
 const filterInstitutions = (searchTerm) => {
   institutions.value = data.filter((inst) =>
@@ -11,10 +12,37 @@ const filterInstitutions = (searchTerm) => {
   );
 };
 
-const addInstitution = () => {};
+const showModal = () => {
+  modal_visibility.value = true;
+};
+
+const closeModal = () => {
+  modal_visibility.value = false;
+};
+
+const submitForm = () => {};
 </script>
 
 <template>
+  <div
+    v-show="modal_visibility"
+    class="flex justify-center items-center absolute top-0 z-50 left-0 w-full h-full"
+  >
+    <div class="bg-red-200 min-w-[50%] min-h-[50%]">
+      <header class="flex justify-end items-center pt-2 pr-2 pb-2 bg-gold">
+        <XMarkIcon class="h-6 w-6 cursor-pointer" @click="closeModal" />
+      </header>
+      <p class="text-3xl heading-montserrat font-bold px-6 py-3 text-center">
+        Edit Institutions
+      </p>
+      <form class="px-10">
+        <FormField label="Institution Name"/>
+        <FormField label="Code"/>
+        <FormField label="Abbreviation"/>
+      </form>
+    </div>
+    <!-- <div class="bg-black bg-opacity-20 w-full h-full absolute" /> -->
+  </div>
   <Header title-text="Institutions" />
   <SearchBar @handle-filter="filterInstitutions" />
   <div class="flex justify-center">
@@ -42,7 +70,7 @@ const addInstitution = () => {};
       button-color="bg-gold"
       type="button"
       class="m-5 ml-8"
-      @click="addInstitution"
+      @click="showModal"
     />
   </div>
 </template>
