9454cec79c0e359afec43f3f22d7537c6e6cc245
unknown <23225186@student.uwa.edu.au>
2022-12-17 14:46:02 +0800

    Add modal

--- components/Modal/index.vue
+++ components/Modal/index.vue
@@ -1,10 +1,29 @@
 <template>
-  <div class="modal-backdrop">
-    <div class="modal">
-      <div class="modal-content">
-        <slot />
-        <button type="button" class="btn-close" @click="handleClose">X</button>
-      </div>
+    <div class="bg-slate-400 bg-opacity-30 flex justify-center items-center fixed top-0 z-50 left-0 w-full h-full">
+        <div class="bg-white min-w-[50%] min-h-[50%] rounded-xl">
+        <header class="flex justify-end items-center pt-2 pr-2 pb-2 bg-gold overflow-hidden">
+            <XMarkIcon class="h-6 w-6 cursor-pointer" @click="closeModal" />
+        </header>
+        <p class="text-3xl heading-montserrat font-bold px-6 py-3 text-center">
+            Add Institution
+        </p>
+        <form class="px-10">
+            <FormField label="Institution Name" />
+            <FormField label="Code" />
+            <FormField label="Abbreviation" />
+        </form>
+        <div class="justify-center">
+            <Button button-text="Submit" button-color="bg-gold" />
+        </div>
+        
+        </div>
     </div>
-  </div>
 </template>
+
+<script setup>
+import { XMarkIcon } from "@heroicons/vue/24/solid";
+const closeModal = () => {
+    this.$emit('Close');
+};
+</script>
+ s
--- pages/admin/institutions.vue
+++ pages/admin/institutions.vue
@@ -3,6 +3,7 @@ import { ref } from "vue";
 import { PencilIcon } from "@heroicons/vue/24/solid";
 import data from "../../data/institutions.json";
 
+
 const institutions = ref(data);
 
 const filterInstitutions = (searchTerm) => {
@@ -11,7 +12,29 @@ const filterInstitutions = (searchTerm) => {
   );
 };
 
-const addInstitution = () => {};
+// Modal function
+let modal_visibility = ref(false);
+
+const showModal = () => {
+  modal_visibility.value = true;
+ 
+};
+const closeModal = () => {
+  modal_visibility.value = false;
+};
+const submitForm = (event) => { 
+  event.preventDefault();
+  const data = new FormData(event.target);
+
+  const value = Object.fromEntries(data.entries());
+  
+  value.topics = data.getAll("topics");
+
+  console.log({ value });
+}
+
+};
+
 </script>
 
 <template>
@@ -42,7 +65,27 @@ const addInstitution = () => {};
       button-color="bg-gold"
       type="button"
       class="m-5 ml-8"
-      @click="addInstitution"
+      @click="showModal"
     />
+  </div>
+  <div v-show="modal_visibility"
+    class="flex justify-center items-center fixed top-0 z-50 left-0 w-full h-full bg-slate-300 bg-opacity-60">
+    <div class="bg-white min-w-[50%] min-h-[50%] rounded-2xl">
+      <header class="flex justify-end items-center pt-2 pr-2 pb-2 bg-gold rounded-t-2xl">
+        <XMarkIcon class="h-6 w-6 cursor-pointer" @click="closeModal" />
+      </header>
+      <p class="text-3xl heading-montserrat font-bold px-6 py-3 text-center">
+        Add Institution
+      </p>
+      <form class="px-10">
+        <FormField label="Institution Name" />
+        <FormField label="Code"  />
+        <FormField label="Abbreviation"  />
+        <div class="flex justify-evenly items-center">
+          <Button button-text="Submit" button-color="bg-gold" type="submit" class="m-5 ml-8" @click="submitForm" />
+          
+        </div>
+      </form>
+    </div>
   </div>
 </template>
