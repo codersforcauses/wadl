69ec72ce964b73f537387fc9cff97707006f7cb0
unknown <23225186@student.uwa.edu.au>
2022-12-17 12:52:52 +0800

    added modal

---
+++ components/Modal/index.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="modal-backdrop">
+        <div class="modal">
+            <div class="modal-content">
+                <slot />
+                <button type="button" class="btn-close" @click="handleClose">X</button>
+            </div>
+        </div>
+    </div>
+</template>
