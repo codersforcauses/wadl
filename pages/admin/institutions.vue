501a04d0505fcd76b592d1ad8b0ffbc9339ccca6
Muffin-Man2 <85885968+Muffin-Man2@users.noreply.github.com>
2022-12-17 14:11:54 +0800

    Modal finished

--- pages/admin/institutions.vue
+++ pages/admin/institutions.vue
@@ -1,10 +1,16 @@
 <script setup>
 import { ref } from "vue";
-import { XMarkIcon } from "@heroicons/vue/24/solid";
+import { XMarkIcon, PencilIcon } from "@heroicons/vue/24/solid";
 import data from "../../data/institutions.json";
 
 const institutions = ref(data);
-var modal_visibility = ref(true);
+var modal_visibility = ref(false);
+
+const form = ref({
+  institution_name: "",
+  code: "",
+  abbreviation: "",
+});
 
 const filterInstitutions = (searchTerm) => {
   institutions.value = data.filter((inst) =>
@@ -12,36 +18,70 @@ const filterInstitutions = (searchTerm) => {
   );
 };
 
+// Launches the modal and fills in with values
+const editInstitution = (institution_name, code, abbreviation) => {
+  form.value.institution_name = institution_name;
+  form.value.code = code;
+  form.value.abbreviation = abbreviation;
+  showModal();
+};
+
+// Launches the modal 
 const showModal = () => {
   modal_visibility.value = true;
 };
 
+// Closes the modal 
 const closeModal = () => {
   modal_visibility.value = false;
 };
 
-const submitForm = () => {};
+// Request sent to server to create or update an institution
+const updateInstitution = () => {
+  console.log("Updating with")
+  console.log(form);
+};
+// Request sent to server to delete an institution
+const deleteInstitution = () => {
+  console.log("Deleting with")
+  console.log(form);
+};
 </script>
 
 <template>
   <div
     v-show="modal_visibility"
-    class="flex justify-center items-center absolute top-0 z-50 left-0 w-full h-full"
+    class="flex justify-center items-center fixed top-0 z-50 left-0 w-full h-full bg-slate-300 bg-opacity-60"
   >
-    <div class="bg-red-200 min-w-[50%] min-h-[50%]">
-      <header class="flex justify-end items-center pt-2 pr-2 pb-2 bg-gold">
+    <div class="bg-white min-w-[50%] min-h-[50%] rounded-2xl">
+      <header class="flex justify-end items-center pt-2 pr-2 pb-2 bg-gold rounded-t-2xl">
         <XMarkIcon class="h-6 w-6 cursor-pointer" @click="closeModal" />
       </header>
       <p class="text-3xl heading-montserrat font-bold px-6 py-3 text-center">
         Edit Institutions
       </p>
       <form class="px-10">
-        <FormField label="Institution Name"/>
-        <FormField label="Code"/>
-        <FormField label="Abbreviation"/>
+        <FormField label="Institution Name" v-model="form.institution_name" />
+        <FormField label="Code" v-model="form.code" />
+        <FormField label="Abbreviation" v-model="form.abbreviation" />
+        <div class="flex justify-evenly items-center">
+          <Button
+            button-text="Save"
+            button-color="bg-gold"
+            type="button"
+            class="m-5 ml-8"
+            @click="updateInstitution"
+          />
+          <Button
+            button-text="Delete"
+            button-color="bg-red-200"
+            type="button"
+            class="m-5 ml-8"
+            @click="deleteInstitution"
+          />
+        </div>
       </form>
     </div>
-    <!-- <div class="bg-black bg-opacity-20 w-full h-full absolute" /> -->
   </div>
   <Header title-text="Institutions" />
   <SearchBar @handle-filter="filterInstitutions" />
@@ -58,9 +98,11 @@ const submitForm = () => {};
         class="justify-between flex px-6 py-2 border-b border-gray-20 items-center"
       >
         <p>{{ inst.name }}</p>
-        <NuxtLink>
-          <button><PencilIcon class="h-4 w-4" /></button>
-        </NuxtLink>
+        <button
+          @click="editInstitution(inst.name, inst.code, inst.name.slice(0, Math.min(3,inst.name.length)))"
+        >
+          <PencilIcon class="h-4 w-4" />
+        </button>
       </li>
     </ul>
   </div>
@@ -70,7 +112,7 @@ const submitForm = () => {};
       button-color="bg-gold"
       type="button"
       class="m-5 ml-8"
-      @click="showModal"
+      @click="editInstitution('', '', '')"
     />
   </div>
 </template>
