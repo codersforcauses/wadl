f5b18805a3c7ba66cc7fb89edddd5e977c5ce41b
unknown <23225186@student.uwa.edu.au>
2023-01-06 02:47:41 +0800

    Seperating the modal in a different file

---
+++ components/Modal/Modal.vue
@@ -0,0 +1,23 @@
+<template>
+    <div v-show="modalVisibility"
+        class="flex justify-center items-center fixed top-0 z-50 left-0 w-full h-full bg-slate-300 bg-opacity-60">
+        <div class="bg-white min-w-[50%] min-h-[50%] rounded-2xl">
+            <header class="flex justify-end items-center pt-2 pr-2 pb-2 bg-gold rounded-t-2xl">
+                <XMarkIcon class="h-6 w-6 cursor-pointer" @click = "close" />
+            </header>
+            <slot />
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { XMarkIcon } from "@heroicons/vue/24/solid";
+import { defineProps, defineEmits } from 'vue'
+const props = defineProps({
+    modalVisibility: { type: Boolean, default: false },
+});
+const emit = defineEmits(['close'])
+const close = ()=> {
+    emit('close')
+};
+</script>
--- pages/admin/institutions.vue
+++ pages/admin/institutions.vue
@@ -1,10 +1,11 @@
 <script setup>
 import { ref } from "vue";
-import { XMarkIcon, PencilIcon } from "@heroicons/vue/24/solid";
+import {  PencilIcon } from "@heroicons/vue/24/solid";
 import data from "../../data/institutions.json";
 import { useInstitutionStore } from '../../stores/institutions';
+import Modal from "../../components/Modal/Modal.vue"
 
-// const institutions = ref(data);
+const institutions = ref(data);
 
 const filterInstitutions = (searchTerm) => {
   institutions.value = data.filter((inst) =>
@@ -15,12 +16,8 @@ const filterInstitutions = (searchTerm) => {
 // Modal
 const modalVisibility = ref(false);
 
-const showModal = () => {
-  modalVisibility.value = true;
-};
-
-const closeModal = () => {
-  modalVisibility.value = false;
+const toggleModal = () => {
+  modalVisibility.value = !modalVisibility.value;
 };
 
 // Pinia
@@ -39,24 +36,20 @@ const CreateInst = (e) => {
 
 <template>
   <!-- Modal -->
-  <div v-show="modalVisibility" class="flex justify-center items-center fixed top-0 z-50 left-0 w-full h-full bg-slate-300 bg-opacity-60">
-    <div class="bg-white min-w-[50%] min-h-[50%] rounded-2xl">
-      <header class="flex justify-end items-center pt-2 pr-2 pb-2 bg-gold rounded-t-2xl">
-        <XMarkIcon class="h-6 w-6 cursor-pointer" @click="closeModal" />
-      </header>
-      <p class="text-3xl heading-montserrat font-bold px-6 py-3 text-center">
-        Add Institutions
-      </p>
-      <form class="px-10" @submit.prevent="CreateInst">
-        <FormField v-model="inst_input.name" label="Institution Name" />
-        <FormField v-model="inst_input.code" label="Code" />
-        <FormField v-model="inst_input.abbreviation" label="Abbreviation" />
-        <div class="flex justify-evenly items-center">
-          <Button button-text="Submit" button-color="bg-gold" type="Submit" class="m-5 ml-8" />
-        </div>
-      </form>
+  <Modal @close="toggleModal" :modal-visibility="modalVisibility">
+  <p class="text-3xl heading-montserrat font-bold px-6 py-3 text-center">
+    Add Institutions
+  </p>
+  <form class="px-10" @submit.prevent="CreateInst">
+    <FormField v-model="inst_input.name" label="Institution Name" />
+    <FormField v-model="inst_input.code" label="Code" />
+    <FormField v-model="inst_input.abbreviation" label="Abbreviation" />
+    <div class="flex justify-evenly items-center">
+      <Button button-text="Submit" button-color="bg-gold" type="Submit" class="m-5 ml-8" />
     </div>
-  </div>
+  </form>
+  </Modal>
+
 
   <Header title-text="Institutions" />
   <SearchBar @handle-filter="filterInstitutions" />
@@ -85,7 +78,7 @@ const CreateInst = (e) => {
       button-color="bg-gold"
       type="button"
       class="m-5 ml-8"
-      @click="showModal"
+      @click="toggleModal"
     />
 
   </div>
