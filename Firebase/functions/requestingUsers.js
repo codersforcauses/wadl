1f70865863d7a3695f285a41aa78ccffccdf25d7
dominictdavies <dominictdavies@gmail.com>
2023-01-07 13:49:19 +0800

    Fixed requestingUsers

--- Firebase/functions/requestingUsers.js
+++ Firebase/functions/requestingUsers.js
@@ -2,15 +2,15 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 admin.initializeApp();
 
-exports.requestingUsers = functions.https.onRequest((req, res) => {
+exports.requestingUsers = functions.https.onRequest(async (req, res) => {
   let information = [];
   const db = admin.firestore();
-  db.collection("users")
+  await db
+    .collection("users")
     .where("requesting", "==", true)
     .get()
     .then((snapshot) => {
       snapshot.forEach((element) => {
-        // console.log(element.data());
         const newElement = {
           id: element.data().id,
           first_name: element.data().first_name,
@@ -20,18 +20,8 @@ exports.requestingUsers = functions.https.onRequest((req, res) => {
           role: element.data().role,
           last_name: element.data().surname,
         };
-        // information = information.concat(newElement);
         console.log(information.push(newElement));
       });
     });
-  // console.log(information);
-  // const result = JSON.parse(information);
-  // console.log(typeof result);
-  res.json("helo");
+  res.json(information);
 });
-
-// exports.requestingUsers = functions.https.onCall((data, context) => {
-//   // ...
-//   const uid = context.auth;
-//   return "hello world";
-// });
