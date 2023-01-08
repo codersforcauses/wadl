357b6033b0a753ac1d6319b61cf55ca6097dae00
nicodeas <nicholas@theugroup.co>
2023-01-08 20:53:11 +0800

    style: lint fix

--- pages/admin/institutions.vue
+++ pages/admin/institutions.vue
@@ -43,27 +43,38 @@ const createInstitution = () => {
 
 <template>
   <Teleport to="body">
-    <Modal :modal-visibility="modalVisibility" @close="
-      () => {
-        modalVisibility = false;
-        resetFormState();
-      }
-    ">
+    <Modal
+      :modal-visibility="modalVisibility"
+      @close="
+        () => {
+          modalVisibility = false;
+          resetFormState();
+        }
+      "
+    >
       <div v-if="editMode">
         <p class="text-3xl heading-montserrat font-bold px-6 py-3 text-center">
           Edit Institution
         </p>
-        <form class="px-10" @submit.prevent="
-          () => {
-            modalVisibility = false;
-            updateInstitution();
-          }
-        ">
+        <form
+          class="px-10"
+          @submit.prevent="
+            () => {
+              modalVisibility = false;
+              updateInstitution();
+            }
+          "
+        >
           <FormField v-model="formInput.name" label="Institution Name" />
           <FormField v-model="formInput.code" label="Code" />
           <FormField v-model="formInput.abbreviation" label="Abbreviation" />
           <div class="flex justify-evenly items-center">
-            <Button button-text="Update" button-color="bg-gold" type="Submit" class="m-5 ml-8" />
+            <Button
+              button-text="Update"
+              button-color="bg-gold"
+              type="Submit"
+              class="m-5 ml-8"
+            />
           </div>
         </form>
       </div>
@@ -71,17 +82,25 @@ const createInstitution = () => {
         <p class="text-3xl heading-montserrat font-bold px-6 py-3 text-center">
           Add Institution
         </p>
-        <form class="px-10" @submit.prevent="
-          () => {
-            modalVisibility = false;
-            createInstitution();
-          }
-        ">
+        <form
+          class="px-10"
+          @submit.prevent="
+            () => {
+              modalVisibility = false;
+              createInstitution();
+            }
+          "
+        >
           <FormField v-model="formInput.name" label="Institution Name" />
           <FormField v-model="formInput.code" label="Code" />
           <FormField v-model="formInput.abbreviation" label="Abbreviation" />
           <div class="flex justify-evenly items-center">
-            <Button button-text="Submit" button-color="bg-gold" type="Submit" class="m-5 ml-8" />
+            <Button
+              button-text="Submit"
+              button-color="bg-gold"
+              type="Submit"
+              class="m-5 ml-8"
+            />
           </div>
         </form>
       </div>
@@ -93,26 +112,37 @@ const createInstitution = () => {
   <div class="flex justify-center">
     <ul class="bg-white rounded-lg w-11/12">
       <li
-        class="text-xl heading-montserrat font-bold px-6 py-3 border-b border-gray-20 rounded-t-lg flex justify-between items-center">
+        class="text-xl heading-montserrat font-bold px-6 py-3 border-b border-gray-20 rounded-t-lg flex justify-between items-center"
+      >
         Institutions
       </li>
-      <li v-for="institution in store.institutions" :key="institution.id"
-        class="justify-between flex px-6 py-2 border-b border-gray-20 items-center">
+      <li
+        v-for="institution in store.institutions"
+        :key="institution.id"
+        class="justify-between flex px-6 py-2 border-b border-gray-20 items-center"
+      >
         <p>{{ institution.name }}</p>
-        <button @click="
-          () => {
-            formInput = institution;
-            editMode = true;
-            modalVisibility = true;
-          }
-        ">
+        <button
+          @click="
+            () => {
+              formInput = institution;
+              editMode = true;
+              modalVisibility = true;
+            }
+          "
+        >
           <PencilIcon class="h-4 w-4" />
         </button>
       </li>
     </ul>
   </div>
   <div class="fixed inset-x-0 bottom-0 w-full bg-white">
-    <Button button-text="Add Institutions" button-color="bg-gold" type="button" class="m-5 ml-8"
-      @click="modalVisibility = true" />
+    <Button
+      button-text="Add Institutions"
+      button-color="bg-gold"
+      type="button"
+      class="m-5 ml-8"
+      @click="modalVisibility = true"
+    />
   </div>
 </template>
