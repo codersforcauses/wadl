1b207d2b193fb01e08d1367af0905dd07f9456fe
nicodeas <nicholas@theugroup.co>
2023-01-08 20:52:36 +0800

    refactor: v-for to use actual id instead of index

--- pages/admin/institutions.vue
+++ pages/admin/institutions.vue
@@ -15,7 +15,7 @@ const modalVisibility = ref(false);
 const editMode = ref(false);
 const formInput = ref({ ...defaultInputState });
 
-const instStore = useInstitutionStore();
+const store = useInstitutionStore();
 
 const resetFormState = () => {
   formInput.value = { ...defaultInputState };
@@ -30,52 +30,40 @@ const filterInstitutions = (searchTerm) => {
 
 const updateInstitution = () => {
   // update store
-  instStore.editInstitution(formInput.value);
+  store.editInstitution(formInput.value);
   resetFormState();
 };
 
 const createInstitution = () => {
   // update store
-  instStore.createInstitution(formInput.value);
+  store.createInstitution(formInput.value);
   resetFormState();
 };
 </script>
 
 <template>
   <Teleport to="body">
-    <Modal
-      :modal-visibility="modalVisibility"
-      @close="
-        () => {
-          modalVisibility = false;
-          resetFormState();
-        }
-      "
-    >
+    <Modal :modal-visibility="modalVisibility" @close="
+      () => {
+        modalVisibility = false;
+        resetFormState();
+      }
+    ">
       <div v-if="editMode">
         <p class="text-3xl heading-montserrat font-bold px-6 py-3 text-center">
           Edit Institution
         </p>
-        <form
-          id="Inst"
-          class="px-10"
-          @submit.prevent="
-            () => {
-              modalVisibility = false;
-              updateInstitution();
-            }
-          "
-        >
+        <form class="px-10" @submit.prevent="
+          () => {
+            modalVisibility = false;
+            updateInstitution();
+          }
+        ">
           <FormField v-model="formInput.name" label="Institution Name" />
           <FormField v-model="formInput.code" label="Code" />
           <FormField v-model="formInput.abbreviation" label="Abbreviation" />
           <div class="flex justify-evenly items-center">
-            <Button
-              button-text="Update"
-              button-color="bg-gold"
-              type="Submit"
-              class="m-5 ml-8"
-            />
+            <Button button-text="Update" button-color="bg-gold" type="Submit" class="m-5 ml-8" />
           </div>
         </form>
       </div>
@@ -83,26 +71,17 @@ const createInstitution = () => {
         <p class="text-3xl heading-montserrat font-bold px-6 py-3 text-center">
           Add Institution
         </p>
-        <form
-          id="Inst"
-          class="px-10"
-          @submit.prevent="
-            () => {
-              modalVisibility = false;
-              createInstitution();
-            }
-          "
-        >
+        <form class="px-10" @submit.prevent="
+          () => {
+            modalVisibility = false;
+            createInstitution();
+          }
+        ">
           <FormField v-model="formInput.name" label="Institution Name" />
           <FormField v-model="formInput.code" label="Code" />
           <FormField v-model="formInput.abbreviation" label="Abbreviation" />
           <div class="flex justify-evenly items-center">
-            <Button
-              button-text="Submit"
-              button-color="bg-gold"
-              type="Submit"
-              class="m-5 ml-8"
-            />
+            <Button button-text="Submit" button-color="bg-gold" type="Submit" class="m-5 ml-8" />
           </div>
         </form>
       </div>
@@ -114,37 +93,26 @@ const createInstitution = () => {
   <div class="flex justify-center">
     <ul class="bg-white rounded-lg w-11/12">
       <li
-        class="text-xl heading-montserrat font-bold px-6 py-3 border-b border-gray-20 rounded-t-lg flex justify-between items-center"
-      >
+        class="text-xl heading-montserrat font-bold px-6 py-3 border-b border-gray-20 rounded-t-lg flex justify-between items-center">
         Institutions
       </li>
-      <li
-        v-for="(inst, idx) in instStore.institutions"
-        :key="idx"
-        class="justify-between flex px-6 py-2 border-b border-gray-20 items-center"
-      >
-        <p>{{ inst.name }}</p>
-        <button
-          @click="
-            () => {
-              formInput = inst;
-              editMode = true;
-              modalVisibility = true;
-            }
-          "
-        >
+      <li v-for="institution in store.institutions" :key="institution.id"
+        class="justify-between flex px-6 py-2 border-b border-gray-20 items-center">
+        <p>{{ institution.name }}</p>
+        <button @click="
+          () => {
+            formInput = institution;
+            editMode = true;
+            modalVisibility = true;
+          }
+        ">
           <PencilIcon class="h-4 w-4" />
         </button>
       </li>
     </ul>
   </div>
   <div class="fixed inset-x-0 bottom-0 w-full bg-white">
-    <Button
-      button-text="Add Institutions"
-      button-color="bg-gold"
-      type="button"
-      class="m-5 ml-8"
-      @click="modalVisibility = true"
-    />
+    <Button button-text="Add Institutions" button-color="bg-gold" type="button" class="m-5 ml-8"
+      @click="modalVisibility = true" />
   </div>
 </template>
