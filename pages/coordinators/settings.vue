eec512b8226f7c5f67552a31e5364dae1b767ef1
unknown <23225186@student.uwa.edu.au>
2023-01-11 00:26:06 +0800

    Adding sidebar structure

---
+++ pages/coordinators/settings.vue
@@ -0,0 +1,34 @@
+<template>
+<aside class="hidden sm:block bg-gray-200 w-72 overflow-y-auto h-screen">
+    <a class="flex items-center space-x-2 rounded-md px-2 py-3 hover:bg-gray-100">
+        <span class="text-2xl"></span>
+        <span>Home</span>
+    </a>
+    <a class="flex items-center space-x-2 rounded-md px-2 py-3 hover:bg-gray-100">
+        <span class="text-2xl"></span>
+        <span>Teams</span>
+    </a>
+    <a class="flex items-center space-x-2 rounded-md px-2 py-3 hover:bg-gray-100">
+        <span class="text-2xl"></span>
+        <span>Draw</span>
+    </a>
+    <a class="flex items-center space-x-2 rounded-md px-2 py-3 hover:bg-gray-100">
+        <span class="text-2xl"></span>
+        <span>User Information</span>
+    </a>
+    <a class="flex items-center space-x-2 rounded-md px-2 py-3 hover:bg-gray-100">
+        <span class="text-2xl"></span>
+        <span>Change Password</span>
+    </a>
+    <a class="flex items-center space-x-2 rounded-md px-2 py-3 hover:bg-gray-100">
+        <span class="text-2xl"></span>
+        <span>Institution</span>
+    </a>
+
+</aside>
+
+</template>
+
+<script setup>
+
+</script>
