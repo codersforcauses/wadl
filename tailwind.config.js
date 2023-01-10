7142d46707b815e6eebaa96a0e20c0cce2bf72b2
ChantelleKerr <chantellekerr93@gmail.com>
2023-01-10 23:32:43 +0800

    remove admin from tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./admin/**/*.vue",
     "./components/**/*.vue",
     "./layouts/**/*.vue",
     "./pages/**/*.vue",
