59d142a05816595fb4e62f8a8d1c46b5f0f81e13
benvarian <88700858+benvarian@users.noreply.github.com>
2023-01-12 12:06:40 +0800

    cleanup

--- middleware/auth.global.js
+++ middleware/auth.global.js
@@ -2,8 +2,8 @@ import { useUserStore } from "../stores/auth";
 
 export default defineNuxtRouteMiddleware(async (to) => {
   const authStore = useUserStore();
-  console.log("global middleware ran", to, authStore);
-  if (to.path.includes("admin") && authStore.role !== "Admin") {
+  // console.log("global middleware ran", to, authStore);
+  if (to.path.includes("admin")  && authStore.role !== "Admin") {
     navigateTo({ path: "/" });
   }
 
