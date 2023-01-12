1ca1fca982cb3fe8bf04d8a153e8fb4be6588c19
benvarian <88700858+benvarian@users.noreply.github.com>
2023-01-12 11:46:17 +0800

    protects all nested admin pages

--- middleware/auth.global.js
+++ middleware/auth.global.js
@@ -1,10 +1,10 @@
 import { useUserStore } from "../stores/auth";
 
 export default defineNuxtRouteMiddleware(async (to) => {
-  // return await navigateTo("/");
   const authStore = useUserStore();
   console.log("global middleware ran", to, authStore);
-  if (to.path === "/admin" && authStore.role !== "Admin") {
+  if (to.path.includes("admin") && authStore.role !== "Admin") {
     navigateTo({ path: "/" });
   }
+
 });
