a1663738d8bd1aff740a15baf6518ab9ccc3bf18
benvarian <88700858+benvarian@users.noreply.github.com>
2023-01-14 21:28:23 +0800

    cleanup

--- Firebase/functions/index.js
+++ Firebase/functions/index.js
@@ -1,10 +1,4 @@
-// const functions = require("firebase-functions");
 const killswitch = require("./billingKillswitch");
-const requestingUsers = require("./requestingUsers");
 const requestingState = require("./requestingState");
 exports.killswitch = killswitch;
-exports.requestingUsers = requestingUsers;
 exports.requestingState = requestingState;
-
-// // Create and deploy your first functions
-// // https://firebase.google.com/docs/functions/get-started
--- plugins/3.firebase-functions-emulator.client.js
+++ plugins/3.firebase-functions-emulator.client.js
@@ -5,10 +5,8 @@ import { defineNuxtPlugin, useRuntimeConfig } from "#imports";
 export default defineNuxtPlugin(async (nuxtApp) => {
   // contains nuxt env vars
   const config = useRuntimeConfig();
-
-  const firebaseApp = getApp(); // app initialized in 1.firebase-init.js
-
   if (config.firebaseMode === "dev") {
+    const firebaseApp = getApp();
     const functions = getFunctions(firebaseApp);
     connectFunctionsEmulator(functions, "localhost", 5001);
     console.log("emulating functions");
