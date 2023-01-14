f859a3a363df026177a778f7d5dfba6aad47fbe8
benvarian <88700858+benvarian@users.noreply.github.com>
2023-01-14 14:58:05 +0800

    make it so it runs on client only

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,13 +2,15 @@ import { useUserStore } from "../stores/auth";
 
 export default defineNuxtRouteMiddleware(async (to, from) => {
   // TODO: make safe :) rm -rf /hacker(wo)mans
-  const authStore = useUserStore();
-  console.log(authStore.$state.role);
-  // const x = document.cookie;
-  console.log(to, from);
-  // console.log("global middleware ran", to, authStore);
-  if (authStore.role !== "Admin") {
-    console.log("admin");
-    await navigateTo("/");
+  if (process.client) {
+    const authStore = useUserStore();
+    console.log(to, from);
+    console.log(authStore.$state.role);
+    if (authStore.role === "Admin") {
+      console.log("admin");
+    } else if (authStore.role !== "Admin") {
+      console.log("not");
+      return navigateTo({ path: "/login" });
+    }
   }
 });
