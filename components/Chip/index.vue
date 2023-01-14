c4615d7eb34ee146538e8c73bd26bc4758f30d62
Nicodeas <nicholas.hy.michlin@gmail.com>
2023-01-14 21:14:53 +0800

    fix(chip): remove unused variable causing warnings

--- components/Chip/index.vue
+++ components/Chip/index.vue
@@ -49,10 +49,7 @@ const chipSize = computed(() => {
       "
     >
       <div class="hover:bg-amber-500 transition rounded-lg">
-        <XMarkIcon
-          class="h-3 w-3 sm:h-4 sm:w-4 lg:h-5 lg:w-5"
-          :class="`${buttonSize}`"
-        />
+        <XMarkIcon class="h-3 w-3 sm:h-4 sm:w-4 lg:h-5 lg:w-5" />
       </div>
     </button>
   </span>
