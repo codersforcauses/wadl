323d91efd77b11332b81538ae364d026b629b050
ChantelleKerr <chantellekerr93@gmail.com>
2023-01-16 16:44:14 +0800

    updated table with more fields

--- pages/coordinator/index.vue
+++ pages/coordinator/index.vue
@@ -1,12 +1,46 @@
 <script setup>
 import ProfileInfo from "~/components/admin/ProfileInfo.vue";
-import Table from "~/components/Coordinator/Table.vue";
+// import Table from "~/components/Coordinator/Table.vue";
+import { useTeamStore } from "../../stores/teams";
+const store = useTeamStore();
+
+const headers = [
+  {
+    key: "name",
+    title: "Team",
+  },
+  {
+    key: "level",
+    title: "Level",
+  },
+  {
+    key: "division",
+    title: "Div",
+  },
+  {
+    key: "timeslot",
+    title: "Timeslot",
+  },
+  {
+    key: "venPref1",
+    title: "Venue Pref 1",
+  },
+  {
+    key: "venPref2",
+    title: "Venue Pref 2",
+  },
+  {
+    key: "venPref3",
+    title: "Venue Pref 3",
+  },
+];
 </script>
 
 <template>
   <div>
     <ProfileInfo username="Carmen Leong" role="Admin" />
-    <Table :headings="['Teams', 'Level', 'Division']" />
+    <!-- <Table :headings="['Teams', 'Level', 'Division']" /> -->
+    <Table :headers="headers" :data="store.teams" />
     <div class="fixed inset-x-0 bottom-0 w-full">
       <div class="flex flex-row gap-4 m-5 justify-left">
         <Button
