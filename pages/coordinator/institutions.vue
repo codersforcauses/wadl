a5c00b1cf2d60c9759e4d0af057923fa43d7154a
Muffin-Man2 <85885968+Muffin-Man2@users.noreply.github.com>
2023-01-18 16:13:03 +0800

    Added confirm popup modal

--- pages/coordinator/institutions.vue
+++ pages/coordinator/institutions.vue
@@ -1,5 +1,29 @@
 <template>
   <section>
+    <Modal :modal-visibility="modalVisible">
+      <h1 class="pb-2 text-2xl text-center">Confirm Changes</h1>
+      <ul class="mx-6" v-for="update in instUpdates">
+        <li>
+          {{ `${update.key} changed to ${update.update}` }}
+        </li>
+      </ul>
+      <div class="flex justify-between items-center">
+        <Button
+          button-text="Reject"
+          button-color="bg-red-500"
+          type="Submit"
+          class="m-5 ml-8"
+          @click="handleReject"
+        />
+        <Button
+          button-text="Accept"
+          button-color="bg-green-500"
+          type="Submit"
+          class="m-5 ml-8"
+          @click="handleUpdate"
+        />
+      </div>
+    </Modal>
     <Header title-text="Institution Settings" />
     <form
       class="bg-white heading-montserrat px-2 md:w-6/12 my-12 flex justify-center flex-col mx-auto"
@@ -54,6 +78,8 @@ import { useInstitutionStore } from "../../stores/institutions";
 
 const store = useInstitutionStore();
 const institutions = ref(null);
+const modalVisible = ref(false);
+const instUpdates = ref([]);
 
 const form = ref({
   id: "",
@@ -64,8 +90,50 @@ const form = ref({
   schoolCode: "",
 });
 const handleTeamJoin = () => {
+  const original_data = institutions.value.filter(
+    (inst) => inst.id === form.value.id
+  )[0];
+  let updates = [];
+  if (form.value.schoolName !== original_data.name) {
+    updates.push({ key: "School Name", update: form.value.schoolName });
+  }
+  if (form.value.schoolNumber !== original_data.phoneNumber) {
+    updates.push({ key: "School Number", update: form.value.schoolNumber });
+  }
+  if (form.value.schoolEmail !== original_data.email) {
+    updates.push({ key: "School Email", update: form.value.schoolEmail });
+  }
+  if (form.value.schoolAbbreviation !== original_data.abbreviation) {
+    updates.push({
+      key: "School Abbreviation",
+      update: form.value.schoolAbbreviation,
+    });
+  }
+  if (form.value.schoolCode !== original_data.code) {
+    updates.push({ key: "School Code", update: form.value.schoolCode });
+  }
+  if (updates.length !== 0) {
+    instUpdates.value = updates;
+    modalVisible.value = true;
+  } else {
+    // might need an alert or warning
+    console.log("No changes");
+  }
+};
+
+const handleUpdate = () => {
   store.checkInstitution(form.value);
   store.updateProfile(form.value);
+  modalVisible.value = false;
+};
+
+const handleReject = () => {
+  const original_data = institutions.value.filter(
+    (inst) => inst.id === form.value.id
+  )[0];
+  console.log(original_data);
+  getInfo(original_data);
+  modalVisible.value = false;
 };
 
 onMounted(async () => {
@@ -75,6 +143,7 @@ onMounted(async () => {
 onUnmounted(async () => {
   await store.clearStore();
 });
+
 const getInfo = (data) => {
   form.value.id = data.id;
   form.value.schoolName = data.name;
