c40d3d8be847e16a2f567a09db3fbdb495d775e0
David <david.holt2002@gmail.com>
2023-01-20 17:32:35 +0800

    Changed position need better styling

--- pages/admin/institutions.vue
+++ pages/admin/institutions.vue
@@ -107,7 +107,16 @@ const createInstitution = () => {
   </Modal>
 
   <Header title-text="Institutions" />
-  <SearchBar @handle-filter="filterInstitutions" />
+  <div class="flex items-center">
+    <SearchBar @handle-filter="filterInstitutions" />
+    <Button
+      button-text="+"
+      button-color="bg-gold"
+      type="button"
+      class="m-1"
+      @click="modalVisibility = true"
+    />
+  </div>
   <div class="flex justify-center">
     <ul class="bg-white rounded-lg w-11/12">
       <li
@@ -134,14 +143,5 @@ const createInstitution = () => {
         </button>
       </li>
     </ul>
-  </div>
-  <div class="fixed inset-x-0 bottom-0 w-full bg-white">
-    <Button
-      button-text="Add Institutions"
-      button-color="bg-gold"
-      type="button"
-      class="m-5 ml-8"
-      @click="modalVisibility = true"
-    />
   </div>
 </template>
