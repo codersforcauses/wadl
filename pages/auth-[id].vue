fa5aed6cd5af8abee7820581c0c7304adf34db0e
NicholasJDavies <nicholasjohndavies42@gmail.com>
2023-01-21 22:01:07 +0800

    added auth-[id] ssr authentication page

---
+++ pages/auth-[id].vue
@@ -0,0 +1,11 @@
+<script setup>
+// import { definePageMeta } from "#imports";
+// const idToken = $route.params.id;
+definePageMeta({
+  middleware: ["auth"],
+});
+</script>
+
+<template>
+  <div class="text-xl">{{ $route.params.id }}</div>
+</template>
