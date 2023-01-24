5d4e18f392b1b80b91d12f62b252a196ba15c095
Ivan <ivan_peh@hotmail.com>
2023-01-24 23:16:19 +0800

    exportTeams now works with firestore

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -14,38 +14,33 @@ import {
 } from "@heroicons/vue/24/outline";
 import { useUserStore } from "../../stores/auth";
 
-import { useInstitutionStore } from "../../stores/institutions";
+import { useTeamStore } from "../../stores/teams";
 
 const user = useUserStore();
 
-const store = useInstitutionStore();  
-
-const exportTeams = () => {
-
-  // retreiving teams data from store
+const exportTeams = async () => {
+  const store = useTeamStore();
+  await store.getTeams();
   const teams = store.teams;
 
-  // converting teams list to csv format
-  var teams_list = [];
+  let teamsList = [];
   const headings = Object.keys(teams[0]);
-  teams_list.push(headings.join(','));
+  teamsList.push(headings.join(","));
 
-  for (let team of teams) {
-  teams_list.push(Object.values(team).join(","));
+  for (const team of teams) {
+    const row = '"'.concat(Object.values(team).join('","'), '"');
+    teamsList.push(row);
   }
 
-  teams_list = teams_list.join("\n");
+  teamsList = teamsList.join("\n");
 
-  // downlaoding file as csv
-  // https://www.geeksforgeeks.org/how-to-create-and-download-csv-file-in-javascript/
-  const blob   = new Blob([teams_list], { type: 'text/csv' });
-  const url    = window.URL.createObjectURL(blob)
-  const anchor = document.createElement('a')
-  anchor.setAttribute('href', url)
-  anchor.setAttribute('download', 'WADL Teams List.csv');
-  anchor.click()
+  const blob = new Blob([teamsList], { type: "text/csv" });
+  const url = window.URL.createObjectURL(blob);
+  const anchor = document.createElement("a");
+  anchor.setAttribute("href", url);
+  anchor.setAttribute("download", "WADL Teams List.csv");
+  anchor.click();
 };
-
 </script>
 
 <template>
@@ -66,7 +61,11 @@ const exportTeams = () => {
       <AdminButton title="Teams" link="/" :icon="UserGroupIcon" />
       <AdminButton title="Venues" link="admin/venues" :icon="MapPinIcon" />
       <AdminButton title="Fixtures" link="/" :icon="TableCellsIcon" />
-      <AdminButton title="Export Teams" :icon="ClipboardIcon" v-on:click="exportTeams"/>
+      <AdminButton
+        title="Export Teams"
+        :icon="ClipboardIcon"
+        v-on:click="exportTeams"
+      />
       <AdminButton
         title="Sign-up Requests"
         link="admin/signup-requests"
