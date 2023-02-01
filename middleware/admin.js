f19443b1f867fb656ba6e7984347366bd06feee3
Nicholas Davies <nicholasjohndavies42@gmail.com>
2023-02-01 16:06:34 +0800

    testing middleware

--- middleware/admin.js
+++ middleware/admin.js
@@ -6,10 +6,13 @@ export default defineNuxtRouteMiddleware(async (_to, _from) => {
   // If you are seeing this further down the line, refactor this funciton
   // to run server side for better security.
 
-  if (process.client) {
+  // if (process.client) {
     const userStore = useUserStore();
+
+    console.log(userStore.role)
+    console.log(userStore.email)
     if (userStore.role !== "Admin") {
       return navigateTo({ path: "/" });
     }
-  }
+  // }
 });
