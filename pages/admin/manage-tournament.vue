12b44ad9a49f94fa8d3b889f79444f5e98810974
unknown <23225186@student.uwa.edu.au>
2023-02-04 01:02:24 +0800

    Fix table for round and venue

--- pages/admin/manage-tournament.vue
+++ pages/admin/manage-tournament.vue
@@ -153,15 +153,93 @@
       </div>
     </div>
     <!-- table -->
-    <!-- header -->
-    <div class="grid grid-cols-2 items-center bg-white">
-      <div class="m-2 flex  pt-5 pb-5 font-montserrat font-semibold text-mid-grey text-left text-lg">
-        Round Information
+    <!-- header1 -->
+    <div class="items-center bg-white flex">
+      <div class="m-2 pt-5 pb-5 font-montserrat font-semibold text-mid-grey text-left text-lg">
+        Round Dates
       </div>
       <div>
         <button class="bg-amber-300 p-1 rounded-full text-black" @click="() => {
-                      print('Add New Info');
-                    }">
+                          print('Add New Info Round Dates');
+                        }">
+          <PlusIcon class="w-7 h-7" />
+        </button>
+      </div>
+    </div>
+    <!-- 3grid -->
+    <div class="grid grid-cols-3 gap-4">
+      <!-- col1 -->
+      <div class="h-96 rounded-xl shadow-lg bg-slate-200 items-center">
+        <!-- head -->
+        <div class="grid grid-cols-4 rounded-xl p-5">
+          <div class="m-auto">
+            <div class="flex flex-col items-center">
+            </div>
+          </div>
+          <div class="m-auto col-span-2">
+            <h1 class="font-montserrat text-3xl font-semibold">ROUND 1</h1>
+          </div>
+          <div class="m-auto items-center">
+            <button @click="() => {
+                                      print('Edit Round Date');
+                                    }">
+              <PencilIcon class="w-10 h-10" />
+            </button>
+          </div>
+          <hr class="h-px my-3 bg-mid-grey border-0 col-span-4">
+        </div>
+        <!-- week table -->
+        <div class="grid grid-cols-3 gap-4 px-10">
+          <div>
+            <div class="flex flex-col items-center">
+              <h1 class="text-center font-montserrat font-semibold text-3xl">
+                15/03
+              </h1>
+              <p class="font-montserrat">TUESDAY</p>
+            </div>
+          </div>
+          <div class="m-auto">
+            <h1 class="font-montserrat">WEEK 1</h1>
+          </div>
+          <div>
+            <div class="flex flex-col items-center">
+              <h1 class="text-center font-montserrat font-semibold text-3xl">
+                17/03
+              </h1>
+              <p class="font-montserrat">WEDNESDAY</p>
+            </div>
+          </div>
+          <div>
+            <div class="flex flex-col items-center">
+              <h1 class="text-center font-montserrat font-semibold text-3xl">
+                15/03
+              </h1>
+              <p class="font-montserrat">TUESDAY</p>
+            </div>
+          </div>
+          <div class="m-auto">
+            <h1 class="font-montserrat">WEEK 2</h1>
+          </div>
+          <div>
+            <div class="flex flex-col items-center">
+              <h1 class="text-center font-montserrat font-semibold text-3xl">
+                17/03
+              </h1>
+              <p class="font-montserrat">WEDNESDAY</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- header2 -->
+    <div class="items-center bg-white flex">
+      <div class="m-2 pt-5 pb-5 font-montserrat font-semibold text-mid-grey text-left text-lg">
+        Venues Information
+      </div>
+      <div>
+        <button class="bg-amber-300 p-1 rounded-full text-black" @click="() => {
+                              print('Add New Info Venue Information');
+                            }">
           <PlusIcon class="w-7 h-7" />
         </button>
       </div>
@@ -169,70 +247,65 @@
     <!-- 3 grid -->
     <div class="grid grid-cols-3 gap-4">
     <!-- col1 -->
-      <div class="max-h-96 overflow-scroll overflow-x-hidden rounded-xl shadow-lg bg-slate-200 items-center">
-        <div class="sticky top-0 grid grid-cols-3 bg-slate-200">
-          <!-- head -->
-          <div class="py-6">
-            <div class="flex flex-col  items-center">
+      <div class="h-96 rounded-xl shadow-lg bg-slate-200 items-center">
+        <!-- head -->
+        <div class="grid grid-cols-4 rounded-xl p-5">          
+          <div class="m-auto">
+            <div class="flex flex-col items-center">
               <h1 class="text-center font-montserrat font-semibold text-3xl">
                 1
               </h1>
               <p class="font-montserrat">WEEK</p>
             </div>
           </div>
-          <div class="py-6">
-            <div class="flex flex-col items-center">
-              <h1 class="text-center font-montserrat font-semibold text-3xl">
-                14/03
-              </h1>
-              <p class="font-montserrat">TUESDAY</p>
-            </div>
+          <div class="m-auto col-span-2">
+              <h1 class="font-montserrat text-3xl font-semibold">TUESDAY</h1>
           </div>
-          <div class="py-6">
-            <div class="flex flex-col items-center">
-              <h1 class="text-center font-montserrat font-semibold text-3xl">
-                15/03
-              </h1>
-              <p class="font-montserrat">WEDNESDAY</p>
-            </div>
-          </div>  
-          <hr class="h-px mx-8 bg-mid-grey border-0 col-span-3">
-          <!-- subhead -->
-          <div class="ml-6 pt-5 pb-5 font-montserrat font-semibold text-mid-grey text-left text-lg">
-            VENUES
+          <div class="m-auto items-center">
+            <button @click="() => {
+                              print('Edit Venue Information');
+                            }">
+              <PencilIcon class="w-10 h-10" />
+            </button>
           </div>
         
+          <hr class="h-px my-3 bg-mid-grey border-0 col-span-4">
+        </div>   
+        <!-- subhead -->
+        <div class="max-h-56 overflow-scroll overflow-x-hidden">
+          <div class="w-full sticky top-0 pl-6 pb-5 font-montserrat font-semibold text-mid-grey text-left text-lg bg-slate-200">
+            VENUES
+          </div> 
+          <!-- table -->
+          <table class="w-11/12 m-auto">
+            <tbody>
+              <tr v-for="(row, index) in venue" :key="index"
+                class="even:bg-slate-200 odd:bg-white hover:bg-light-yellow transition duration-150 ease-in-out font-montserrat">
+                <td class="p-2" v-for="(object, ind) in headersVenues"
+                  :key="ind">
+                  {{ row[object.key] }}
+                </td>
+                <td class="text-right p-2">
+                  <button @click="() => {
+                    print('Delete Row');
+                  }">
+                    <XMarkIcon class="w-4 h-4" />
+                  </button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </div>
-        <!-- table -->
-        <table class="w-11/12 m-auto">
-          <tbody>
-            <tr v-for="(row, index) in venue" :key="index"
-              class="even:bg-slate-200 odd:bg-white hover:bg-light-yellow transition duration-150 ease-in-out font-montserrat">
-              <td class="p-2" v-for="(object, ind) in headersVenues"
-                :key="ind">
-                {{ row[object.key] }}
-              </td>
-              <td class="text-right p-2">
-                <button @click="() => {
-                  print('Delete Row');
-                }">
-                  <XMarkIcon class="w-4 h-4" />
-                </button>
-              </td>
-            </tr>
-          </tbody>
-        </table>
       </div>
-
-      
     </div>
+  
   </div>
   
 </template>
 
 <script setup>
 import venue from "../../data/venues.json"
-import { XMarkIcon, PlusIcon } from "@heroicons/vue/24/solid"
+import { XMarkIcon, PlusIcon, PencilIcon } from "@heroicons/vue/24/solid"
 
 const noviceNum = 10;
 const juniorNum = 20;
