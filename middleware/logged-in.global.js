4957cbaffe9bcb0594d5392ff304356852ff2528
Nicholas Davies <nicholasjohndavies42@gmail.com>
2023-02-04 16:16:32 +0800

    fixing error message

--- middleware/logged-in.global.js
+++ middleware/logged-in.global.js
@@ -2,10 +2,12 @@ import { useUserStore } from "../stores/user";
 import { defineNuxtRouteMiddleware, navigateTo } from "#imports";
 
 export default defineNuxtRouteMiddleware(async (to, _from) => {
-  if (process.server) {
+  // Running on client only to prevent routing conflicts -- admin 
+  // route and log in route conflicting. Important routes are still protected.
+  if (process.client) {
     const userStore = await useUserStore();
     if (userStore.requesting) {
-      userStore.clearStore();
+      await userStore.clearStore();
     }
 
     const publicRoutes = ["/", "/signup", "/login"];
