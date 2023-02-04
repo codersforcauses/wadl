a98f8d4e93f0665a64bdfd1568133b8bc5542e08
Nicholas Davies <nicholasjohndavies42@gmail.com>
2023-02-04 14:40:42 +0800

    removed console logs

--- server/api/session.post.js
+++ server/api/session.post.js
@@ -1,20 +1,10 @@
-// import { useUserStore } from "../../stores/user"
-// import { useNuxtApp } from "#imports";
-
 import { set } from "vue-demi"
 
 export default defineEventHandler(async (event) => {
-    const body = await readBody(event)
-    // console.log(body.token)
-    // let token = getCookie(event, 'auth-token')
-
-    setCookie(event, 'auth-token', body.token)
-    console.log("cookie is set.")
-    console.log("token = " + body.token[79] + body.token[80] + body.token[81])
-    
-    // console.log(token)
-
-    return {
-      api: 'works'
-    }
-  })
+  
+  const body = await readBody(event)
+  setCookie(event, 'auth-token', body.token)
+  return {
+    api: 'works'
+  }
+})
