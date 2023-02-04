6c30932e5f09379115083377d24928df7a0a5137
Nicholas Davies <nicholasjohndavies42@gmail.com>
2023-02-04 15:27:18 +0800

    cleaned middleware

--- middleware/logged-in.global.js
+++ middleware/logged-in.global.js
@@ -2,21 +2,15 @@ import { useUserStore } from "../stores/user";
 import { defineNuxtRouteMiddleware, navigateTo } from "#imports";
 
 export default defineNuxtRouteMiddleware(async (to, _from) => {
-  // NOTE: Nuxt 3.0.0 does not support server side access to client state
-  // If you are seeing this further down the line, refactor this funciton
-  // to run server side for better security.
+  const userStore = await useUserStore();
+  if (userStore.requesting) {
+    userStore.clearStore();
+  }
 
-  if (process.client) {
-    const userStore = await useUserStore();
-    if (userStore.requesting) {
-      userStore.clearStore();
-    }
+  const publicRoutes = ["/", "/signup", "/login"];
+  if (publicRoutes.includes(to.path)) return;
 
-    const publicRoutes = ["/", "/signup", "/login"];
-    if (publicRoutes.includes(to.path)) return;
-
-    if (userStore.auth === null) {
-      return navigateTo({ path: "/login" });
-    }
+  if (userStore.auth === null) {
+    return navigateTo({ path: "/login" });
   }
 });
