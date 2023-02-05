a2baa62819233f52048d1c65764eb61ddadd3c47
LemonTheSour <m.m.downing@bigpond.com>
2023-02-05 22:25:23 +0800

    dummy data and basic styling

--- components/ViewTeams.vue
+++ components/ViewTeams.vue
@@ -4,7 +4,13 @@
     size="md:1/3"
     @close="emit('close')"
   >
-    <div class="w-full h-48"></div>
+    <div class="w-full h-48">
+      <div class="font-montserrat text-2xl text-center pt-10 px-10">
+        Registered Teams
+      </div>
+      <div class="font-monsterrat text-xl text-center">Junior</div>
+      <hr class="mx-8" />
+    </div>
   </Modal>
 </template>
 
