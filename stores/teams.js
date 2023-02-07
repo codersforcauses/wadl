cb4eae3c5984235325523f7e48d84f63350f628f
ChantelleKerr <chantellekerr93@gmail.com>
2023-02-07 16:51:13 +0800

    gets teams by tournament, level and division

--- stores/teams.js
+++ stores/teams.js
@@ -1,11 +1,12 @@
 import { defineStore } from "pinia";
 import { useNuxtApp } from "#imports";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, query, where } from "firebase/firestore";
 
 export const useTeamStore = defineStore("team", {
   state: () => {
     return {
       teams: [],
+      divisions: [],
     };
   },
   getters: {},
@@ -37,6 +38,39 @@ export const useTeamStore = defineStore("team", {
     },
     async createTeam(team) {
       // const { $db } = useNuxtApp();
+    },
+    async getTeamByTournamentDivision(tournamentId, level, division) {
+      console.log("%^%#$%^#", tournamentId, level, division);
+      this.teams = [];
+      const { $clientFirestore } = useNuxtApp();
+      if (!$clientFirestore) return;
+      const ref = collection($clientFirestore, "teams");
+      const q = query(
+        ref,
+        where("tournamentId", "==", tournamentId),
+        where("level", "==", level),
+        where("division", "==", division)
+      );
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        const team = {
+          id: doc.id,
+          tournamentId: doc.data().tournamentId,
+          institutionId: doc.data().institutionId,
+          name: doc.data().name,
+          level: doc.data().level,
+          division: doc.data().division,
+          timeslot: doc.data().timeslot,
+          hasVenuePreference: doc.data().hasVenuePreference,
+          venuePreference: doc.data().venuePreference,
+          weekPreference: doc.data().weekPreference,
+          allocatedTue: doc.data().allocatedTue,
+          allocatedWed: doc.data().allocatedWed,
+          notes: doc.data().notes,
+        };
+        this.divisions.push(team);
+      });
+      console.log("***", this.divisions);
     },
   },
 });
