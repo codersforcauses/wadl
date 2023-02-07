195099e5bbabf623594640ccf112956248f1261f
David <david.holt2002@gmail.com>
2023-02-07 22:57:40 +0800

    take latest commits

--- components/Stepper.vue
+++ components/Stepper.vue
@@ -1,59 +1,32 @@
 <template>
-  <div class="flex items-center w-1/3">
-    <div class="rounded-full w-6 h-6 bg-light-green">
-      <div
-        v-if="stage === 1"
-        class="flex text-l text-white place-content-center"
-      >
-        1
-      </div>
-      <div v-else>
-        <CheckIcon class="text-white" />
-      </div>
-    </div>
-
-    <div class="w-16 h-px inline-block bg-light-grey" />
-
-    <div
-      :class="`rounded-full w-6 h-6 ${
-        stage < 2 ? 'bg-light-grey' : 'bg-light-green'
-      }`"
-    >
-      <div v-if="stage < 3" class="flex text-l text-white place-content-center">
-        2
-      </div>
-      <div v-else>
-        <CheckIcon class="text-white" />
-      </div>
-    </div>
-
-    <div class="w-16 h-px inline-block bg-light-grey" />
-
-    <div
-      :class="`rounded-full w-6 h-6 ${
-        stage < 3 ? 'bg-light-grey' : 'bg-light-green'
-      }`"
-    >
-      <div v-if="stage < 4" class="flex text-l text-white place-content-center">
-        3
-      </div>
-      <div v-else>
-        <CheckIcon class="text-white" />
-      </div>
-    </div>
-
-    <div class="w-16 h-px inline-block bg-light-grey" />
-
-    <div
-      :class="`rounded-full w-6 h-6 ${
-        stage < 4 ? 'bg-light-grey' : 'bg-light-green'
-      }`"
-    >
-      <div v-if="stage < 5" class="flex text-l text-white place-content-center">
-        4
-      </div>
-      <div v-else>
-        <CheckIcon class="text-white" />
+  <div>
+    <div class="flex items-center w-1/3">
+      <div v-for="(state, index) in stateList" :key="index">
+        <div
+          :class="`rounded-full w-6 h-6 ${
+            stage < index + 1 ? 'bg-light-grey' : 'bg-light-green'
+          }`"
+        >
+          <div
+            v-if="state === stateLength && index === stateLength - 1"
+            class="flex text-l text-white place-content-center"
+          >
+            <CheckIcon class="text-white" />
+          </div>
+          <div v-else>
+            <div
+              v-if="stage < index + 2"
+              class="flex text-l text-white place-content-center"
+            >
+              {{ index + 1 }}
+            </div>
+            <div v-else>
+              <CheckIcon class="text-white" />
+            </div>
+          </div>
+        </div>
+        <div class="w-px h-16 ml-3 bg-light-grey" />
+        <div>{{ state }}</div>
       </div>
     </div>
   </div>
@@ -63,12 +36,7 @@
 import { ref } from "vue";
 import { CheckIcon } from "@heroicons/vue/20/solid";
 
-const props = defineProps({
-  labelText1: { type: String, default: "Open" },
-  labelText2: { type: String, default: "Closed" },
-  labelText3: { type: String, default: "Running" },
-  labelText4: { type: String, default: "Complete" },
-});
-
-const stage = ref(1);
+const stage = ref(4);
+const stateList = ["Open", "Closed", "Running", "Complete"];
+const stateLength = stateList.length;
 </script>
