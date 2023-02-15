a651456cc923f08744d5a5fbe49dbf2f7bf4224b
rezbyte <48037381+rezbyte@users.noreply.github.com>
2023-02-15 15:01:08 +0800

    Use Header component in ViewTeams

--- components/ViewTeams.vue
+++ components/ViewTeams.vue
@@ -5,11 +5,7 @@
     @close="emit('close')"
   >
     <div class="w-full h-48">
-      <div class="font-montserrat text-2xl text-center pt-10 px-10">
-        Registered Teams
-      </div>
-      <div class="font-monsterrat text-xl text-center">Junior</div>
-      <hr class="mx-8" />
+      <Header title-text="Registered Teams" subtitle-text="JUNIOR" />
     </div>
   </Modal>
 </template>
