2e1fb10f34d071d1f930c9126c63291de6fe54bf
David <david.holt2002@gmail.com>
2023-03-20 14:12:16 +0800

    Added adjudicator role

--- middleware/adjudicator.js
+++ middleware/adjudicator.js
@@ -3,9 +3,9 @@ import { defineNuxtRouteMiddleware, navigateTo } from "#imports";
 
 export default defineNuxtRouteMiddleware(async (_to, _from) => {
   if (process.server) {
-    // const userStore = await useUserStore();
-    // if (userStore.role !== "Team Coordinator") {
-    //   return navigateTo({ path: "/" });
-    // }
+    const userStore = await useUserStore();
+    if (userStore.role !== "Ajudicator") {
+      return navigateTo({ path: "/" });
+    }
   }
 });
