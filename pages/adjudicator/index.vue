120b2b1b7d39a9d6be3b3544fe9aae1d2bd80278
David <david.holt2002@gmail.com>
2023-03-20 14:43:06 +0800

    started on adjudicator page

--- pages/adjudicator/index.vue
+++ pages/adjudicator/index.vue
@@ -1,3 +1,11 @@
+<script setup>
+import { useTournamentStore } from "../../stores/tournaments";
+
+import juniorFixtures from "../../data/juniorDraw.json";
+import noviceFixtures from "../../data/noviceDraw.json";
+import seniorFixtures from "../../data/seniorDraw.json";
+</script>
+
 <template>
   <p>hi</p>
 </template>
