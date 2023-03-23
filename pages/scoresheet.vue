56ec4f74a771d4b6be37a09668bf95c70ca9ce9c
ChantelleKerr <chantellekerr93@gmail.com>
2023-03-23 12:27:05 +0800

    Finished UI

--- pages/scoresheet.vue
+++ pages/scoresheet.vue
@@ -1,18 +1,28 @@
 <template>
   <Header title-text="Scoresheet" />
 
-  <div class="flex justify-evenly mx-8 my-4">
-    <p>Division: 1</p>
-    <p>Round: 3</p>
-    <p>Date: 28 March</p>
-    <p>Time: 5:15pm</p>
-    <p>Level: Junior</p>
+  <div class="flex justify-start ml-14 my-4">
+    <p class="mr-4 text-mid-grey">
+      Division: <span class="text-dark-grey">1 </span>
+    </p>
+    <p class="mr-4 text-mid-grey">
+      Round: <span class="text-dark-grey">3</span>
+    </p>
+    <p class="mr-4 text-mid-grey">
+      Date: <span class="text-dark-grey">28 March</span>
+    </p>
+    <p class="mr-4 text-mid-grey">
+      Time: <span class="text-dark-grey">5:15pm</span>
+    </p>
+    <p class="text-mid-grey">
+      Level: <span class="text-dark-grey">Junior</span>
+    </p>
   </div>
 
-  <p class="flex justify-center text-xl font-bold my-4">
+  <p class="flex justify-center text-xl my-4">
     Affirmative Team: Perth College 4
   </p>
-  <div class="flex justify-center">
+  <div class="flex justify-center mx-6">
     <table class="w-full max-w-7xl">
       <tr>
         <th>Names</th>
@@ -23,12 +33,36 @@
         <th>Total</th>
       </tr>
       <tr class="border-b">
-        <td class="text-center">Chantelle</td>
-        <td class="px-4"><FormField placeholder="0" type="number" /></td>
-        <td class="px-4"><FormField placeholder="0" type="number" /></td>
-        <td class="px-4"><FormField placeholder="0" type="number" /></td>
+        <td class="text-center pt-2">
+          <FormField placeholder="Student Name" />
+        </td>
+        <td class="px-4 pt-2"><FormField placeholder="0" type="number" /></td>
+        <td class="px-4 pt-2"><FormField placeholder="0" type="number" /></td>
+        <td class="px-4 pt-2"><FormField placeholder="0" type="number" /></td>
 
-        <td class="px-4"><FormField placeholder="0" type="number" /></td>
+        <td class="px-4 pt-2"><FormField placeholder="0" type="number" /></td>
+        <td class="text-center">75</td>
+      </tr>
+      <tr class="border-b">
+        <td class="text-center pt-2">
+          <FormField placeholder="Student Name" />
+        </td>
+        <td class="px-4 pt-2"><FormField placeholder="0" type="number" /></td>
+        <td class="px-4 pt-2"><FormField placeholder="0" type="number" /></td>
+        <td class="px-4 pt-2"><FormField placeholder="0" type="number" /></td>
+
+        <td class="px-4 pt-2"><FormField placeholder="0" type="number" /></td>
+        <td class="text-center">75</td>
+      </tr>
+      <tr class="border-b">
+        <td class="text-center pt-2">
+          <FormField placeholder="Student Name" />
+        </td>
+        <td class="px-4 pt-2"><FormField placeholder="0" type="number" /></td>
+        <td class="px-4 pt-2"><FormField placeholder="0" type="number" /></td>
+        <td class="px-4 pt-2"><FormField placeholder="0" type="number" /></td>
+
+        <td class="px-4 pt-2"><FormField placeholder="0" type="number" /></td>
         <td class="text-center">75</td>
       </tr>
       <tr>
@@ -36,11 +70,134 @@
         <td></td>
         <td></td>
         <td></td>
-
         <td></td>
         <td class="text-center border-b py-2">75</td>
       </tr>
     </table>
   </div>
+
+  <p class="flex justify-center text-xl mb-4">Negative Team: Mercy College 2</p>
+  <div class="flex justify-center mx-6">
+    <table class="w-full max-w-7xl">
+      <tr class="border-b">
+        <th>Names</th>
+        <th class="w-36">Manner(40)</th>
+        <th class="w-36">Matter(40)</th>
+        <th class="w-36">Method(20)</th>
+        <th class="w-36">POIs</th>
+        <th>Total</th>
+      </tr>
+      <tr class="border-b">
+        <td class="text-center pt-2">
+          <FormField placeholder="Student Name" />
+        </td>
+        <td class="px-4 pt-2"><FormField placeholder="0" type="number" /></td>
+        <td class="px-4 pt-2"><FormField placeholder="0" type="number" /></td>
+        <td class="px-4 pt-2"><FormField placeholder="0" type="number" /></td>
+
+        <td class="px-4 pt-2"><FormField placeholder="0" type="number" /></td>
+        <td class="text-center">75</td>
+      </tr>
+      <tr class="border-b">
+        <td class="text-center pt-2">
+          <FormField placeholder="Student Name" />
+        </td>
+        <td class="px-4 pt-2"><FormField placeholder="0" type="number" /></td>
+        <td class="px-4 pt-2"><FormField placeholder="0" type="number" /></td>
+        <td class="px-4 pt-2"><FormField placeholder="0" type="number" /></td>
+
+        <td class="px-4 pt-2"><FormField placeholder="0" type="number" /></td>
+        <td class="text-center">75</td>
+      </tr>
+      <tr class="border-b">
+        <td class="text-center pt-2">
+          <FormField placeholder="Student Name" />
+        </td>
+        <td class="px-4 pt-2"><FormField placeholder="0" type="number" /></td>
+        <td class="px-4 pt-2"><FormField placeholder="0" type="number" /></td>
+        <td class="px-4 pt-2"><FormField placeholder="0" type="number" /></td>
+
+        <td class="px-4 pt-2"><FormField placeholder="0" type="number" /></td>
+        <td class="text-center">75</td>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td class="text-center border-b py-2">75</td>
+      </tr>
+    </table>
+  </div>
+
+  <div class="w-11/12 flex justify-end pt-6">
+    <Button size="large" button-text="Submit" />
+  </div>
 </template>
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+
+const initalState = {
+  affirmativeTeam: [
+    {
+      student1: {
+        name: "",
+        manner: 0,
+        matter: 0,
+        method: 0,
+        poi: 0,
+        total: 0,
+      },
+      student2: {
+        name: "",
+        manner: 0,
+        matter: 0,
+        method: 0,
+        poi: 0,
+        total: 0,
+      },
+      student3: {
+        name: "",
+        manner: 0,
+        matter: 0,
+        method: 0,
+        poi: 0,
+        total: 0,
+      },
+      total: 0,
+    },
+  ],
+  negativeTeam: [
+    {
+      student1: {
+        name: "",
+        manner: 0,
+        matter: 0,
+        method: 0,
+        poi: 0,
+        total: 0,
+      },
+      student2: {
+        name: "",
+        manner: 0,
+        matter: 0,
+        method: 0,
+        poi: 0,
+        total: 0,
+      },
+      student3: {
+        name: "",
+        manner: 0,
+        matter: 0,
+        method: 0,
+        poi: 0,
+        total: 0,
+      },
+      total: 0,
+    },
+  ],
+};
+
+const scoresheet = ref(initalState);
+</script>
