c4a02978b40933232436e08d26cd9ab2315fb1f4
David <david.holt2002@gmail.com>
2023-03-26 09:36:04 +0800

    Imported table to adjudicator landing page

--- pages/adjudicator/index.vue
+++ pages/adjudicator/index.vue
@@ -1,11 +1,38 @@
-<script setup>
-import { useTournamentStore } from "../../stores/tournaments";
-
-import juniorFixtures from "../../data/juniorDraw.json";
-import noviceFixtures from "../../data/noviceDraw.json";
-import seniorFixtures from "../../data/seniorDraw.json";
-</script>
-
+<!-- eslint-disable vue/attribute-hyphenation -->
 <template>
-  <p>hi</p>
+  <!-- competitions -->
+  <Header title-text="Competitions" />
+  <div
+    v-if="store.getRunning.length > 0"
+    class="mx-auto mt-4 flex justify-center flex-wrap"
+  >
+    <!-- replace key with item.id when final data exists -->
+    <div v-for="tournament in store.getRunning" :key="tournament.id">
+      <NuxtLink :to="`../fixtures/${tournament.id}`">
+        <LevelButton :text="tournament.shortName" />
+      </NuxtLink>
+    </div>
+  </div>
+  <p
+    v-else
+    class="text-xl py-5 text-center text-mid-grey font-montserrat font-light"
+  >
+    There are no running tournaments.
+  </p>
 </template>
+
+<script setup>
+import LevelButton from "../../components/HomePage/LevelButton.vue";
+import { useTournamentStore } from "../../stores/tournaments";
+import { onMounted } from "vue";
+import { useHead } from "#imports";
+
+useHead({
+  title: "WADL",
+});
+const store = useTournamentStore();
+
+onMounted(async () => {
+  await store.getTournaments();
+});
+</script>
