d2831b3a18c882fc7632ed2cb8ce81418bab399f
Nicholas Davies <nicholasjohndavies42@gmail.com>
2023-04-17 14:18:12 +0800

    removed old cookie set method

--- server/api/session.post.js
+++
@@ -1,9 +0,0 @@
-import { defineEventHandler, readBody, setCookie } from "#imports";
-
-export default defineEventHandler(async (event) => {
-  const body = await readBody(event);
-  setCookie(event, "auth-token", body.token);
-  return {
-    api: "works",
-  };
-});
