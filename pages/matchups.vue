b8f3d1e831f38eb9cd09a34a1984a6967019fc9f
benvarian <88700858+benvarian@users.noreply.github.com>
2023-04-17 16:24:57 +0800

    testing

--- pages/matchups.vue
+++ pages/matchups.vue
@@ -1,10 +1,13 @@
 <template>
-    <div>
-
-    </div>
 </template>
 
 <script setup>
-
+import { useMatchupStore } from "../stores/matchups";
+import { onMounted } from "vue";
+const store = await useMatchupStore();
+onMounted(async () => {
+    console.log("getting data");
+    await store.getMatchups();
+    console.log("done");
+});
 </script>
-
