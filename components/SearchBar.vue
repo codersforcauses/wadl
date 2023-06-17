bcfc6c1424fc35016767fc21397e470d92fe0ba7
benvarian <88700858+benvarian@users.noreply.github.com>
2023-06-17 20:29:15 +0800

    fix const issue

--- components/SearchBar.vue
+++ components/SearchBar.vue
@@ -18,5 +18,6 @@
 import { MagnifyingGlassIcon } from "@heroicons/vue/24/solid";
 
 const emit = defineEmits(["handleFilter"]);
-const searchString = "";
+// eslint-disable-next-line prefer-const
+let searchString = "";
 </script>
